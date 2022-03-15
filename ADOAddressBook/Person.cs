7e3d72c631a1e12501d5efc06a83c1045038cc99
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-03-16 00:29:28 +0530

    [Nikhil Kumar]:AddressBook UCs completed using SQL commands

---
+++ ADOAddressBook/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOAddressBook
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+        public string AdrBookName { get; set; }
+    }
+}
---
+++ ADOAddressBook/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                Console.WriteLine(" Welcome to contact management program");
+                ContactOperations co = new ContactOperations();
+
+                ShowOptions();
+
+                void ShowOptions()
+                {
+                    Console.Write("\n Select Option : 1.Display Contacts  \t 2.Edit Contact Details \t");
+                    int option = int.Parse(Console.ReadLine());
+
+
+                    switch (option)
+                    {
+                        case 1:
+                            co.GetContactDetails();
+                            ShowOptions();
+                            break;
+                        case 2:
+                            co.EditContact();
+                            ShowOptions();
+                            break;
+                        default: break;
+                    }
+                }
+            
+        }
+    }
+}
